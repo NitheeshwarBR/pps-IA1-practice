90f44f2b9e3e539be3c698cd915d2c453ae24bea
NITHEESHWAR B R <95460968+NitheeshwarBR@users.noreply.github.com>
2022-02-02 04:29:04 +0000

    p4original.c

--- p4original.c
+++ p4original.c
@@ -1,5 +1,47 @@
 #include<stdio.h>
-float input()
+int input_array_size()
 {
-  printf("")
+  int a;
+  printf("ENTER THE SIZE OF NUMBER\n");
+  scanf("%d",&a);
+  return a;
 }
+void input_array(int n,int a[n])
+{
+  printf("ENTER THE NUMBERS\n");
+  for(int i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+
+}
+int sum_n_arrays(int n,int a[n])
+{
+  int sum=0;
+  for(int i=0;i<n;i++)
+  {
+    sum=sum+a[i];
+  }
+  return sum;
+
+}
+void out_put(int n,int a[n],int sum)
+{
+  for(int i=0;i<n-1;i++)
+  {
+    printf("%d+",a[i]);
+  }
+  printf("%d is %d",a[n-1],sum);
+}
+int main()
+{
+  int x,sum;
+  x=input_array_size();
+  int a[x];
+  input_array(x,a);
+  sum=sum_n_arrays(x,a);
+  out_put(x,a,sum);
+  return 0;
+
+}
+
