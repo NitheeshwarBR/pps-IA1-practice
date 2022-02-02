fd081a6039e734afaa75fd18865469b946934ce4
NITHEESHWAR B R <95460968+NitheeshwarBR@users.noreply.github.com>
2022-02-02 04:12:19 +0000

    p2original.c

--- p3original.c
+++ p3original.c
@@ -2,6 +2,33 @@
 int input_n()
 {
   int n;
-  printf("ENTER THE NUMBER")
+  printf("ENTER THE NUMBER\n");
+  scanf("%d",&n);
+  return n;
+}
+int sum_n(int n)
+{
+  int sum=0;
+  for(int i=1;i<=n;i++)
+  {
+    sum=sum+i;
+  }
+  return sum;
+}
+void output(int n,int sum)
+{
+  for(int i=1;i<n;i++)
+  {
+    printf("%d+",i);
+  }
+  printf("%d is %d\n",n,sum);
+}
+int main()
+{
+  int x,sum;
+  x=input_n();
+  sum=sum_n(x);
+  output(x,sum);
+  return 0;
 }
 
