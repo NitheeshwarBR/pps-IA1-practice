c811210f002f8460e0a3608ffc1250cca3a9f08d
NITHEESHWAR B R <95460968+NitheeshwarBR@users.noreply.github.com>
2022-02-03 08:47:40 +0000

    p2final.c ,p2debuglog.txt

--- p2final.c
+++ p2final.c
@@ -1,1 +1,46 @@
-
+#include<stdio.h>
+int input()
+{
+  int x;
+  printf("ENTER THE NUMBER\n");
+  scanf("%d",&x);
+  return x;
+}
+int cmp(int a,int b,int c)
+{
+  if(a>b&&a>c)
+  {
+    return a;
+  }
+  else if(b>c)
+  {
+    return b;
+  }
+  else{
+    return c;
+  }
+}
+void output(int a,int b,int c,int largest)
+{
+  if(largest==a)
+  {
+    printf("%d is larger than %d and %d\n",a,b,c);
+  }
+  else if(largest==b)
+  {
+    printf("%d is larger than %d and %d\n",b,a,c);
+  }
+  else{
+    printf("%d is larger than %d and %d\n",c,a,b);
+  }
+}
+int main()
+{
+  int a,b,c,compare;
+  a=input();
+  b=input();
+  c=input();
+  compare=cmp(a,b,c);
+  output(a,b,c,compare);
+  return 0;
+}
